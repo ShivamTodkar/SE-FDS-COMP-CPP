3a111d8a6812c65bde41db0fa8dfb726e66ad871
ShyamTodkar <121312340+ShivamTodkar@users.noreply.github.com>
2022-12-28 21:55:31 +0530

    Update postfix.cpp

--- postfix.cpp
+++ postfix.cpp
@@ -1,164 +1,174 @@
 #include<iostream>
-#include<stdlib.h>
-#include<cstring>
 using namespace std;
-int k = 0;//store the length of postfix expression
-class stack// declare stack as ADT
-{
-char st[20];
-int top;
+#define MAX 20
+class stack {
+int top,topeval;
+char infix[MAX], postfix[MAX],stk[MAX];
+float stkeval[MAX];
 public:
-stack()
-{
-top=-1;
-}
-int empty()//check is stack empty or not
-{
-if(top==-1)
-return 1;
-return 0;
-}
-char gettop()//f7unction will return top element of stack
-{
-return st[top];
-}
-void push(char ch)//insert elemnet into stack
-{
-st[++top]=ch;
-}
-char pop()//removeing top value from stack{ if(!empty())
-return st[top--];
-}
+stack();
+void push(char);
+void pusheval(float);
+void read();
+int IsEmpty ();
+int IsEmptyeval ();
+char pop();
+float popeval();
+void covert_infix_postfix();
+int priority(char x);
+float evaluatepostfix();
 };
-// get priority of operators as per precedence
-// higher priority given to operators with higher precedence
-// for non operators, return 0
-int getpriority(char ch)
-{ switch (ch) {
-case '/':
-case '*': return 2;
-case '+':
-case '-': return 1;
-default : return 0;
+stack :: stack () {
+top=-1;
+topeval=-1;
 }
+int stack :: IsEmpty () {
+if (top == -1)
+return (-1);
+else
+return (1);
 }
-// convert infix expression to postfix using a stack
-void infix2postfix(char infix[], char postfix[], int size)
-{
-stack s;
-int priority;
-int i = 0;
-int k = 0;
-char ch;
-while (i < size)//read while infix expression not ended
-{
-ch = infix[i];
-if (ch == '(') {// if left paranthesis occur---simply push the
-opening parenthesis
-s.push(ch);
-i++;
-continue;
+int stack :: IsEmptyeval () {
+if (topeval == -1)
+return (-1);
+else
+return (1);
 }
-if (ch == ')') { // if right parenthesis encountered,
-// pop of all the elements and append it to
-// the postfix expression till we encounter
-// a opening parenthesis
-while (!s.empty() && s.gettop() != '(') {
-postfix[k++] = s.gettop();
-s.pop();
+void stack :: push(char temp) {
+top ++; //take it to empty position
+if (top == MAX) //check if there is any space
+cout<<"Stack is Full";
+else {
+stk [top] = temp; }
 }
-// pop off the opening parenthesis
-also if (!s.empty()) {
-s.pop();}
-i++;
-continue;
+void stack :: pusheval(float temp) {
+topeval ++;
+if (topeval == MAX)
+cout<<"Stack is Full";
+else {
+stkeval [topeval] = temp; }
 }
-priority = getpriority(ch);
-if (priority == 0) { // we saw an operand
-// simply append it to postfix expression
-postfix[k++] = ch;
-}
-else { // we saw an operator
-if (s.empty()) { // simply push the operator onto stack
-if
-// stack is empty
-s.push(ch);
+char stack :: pop() {
+int status;
+char temp;
+status = IsEmpty ();
+if (status == -1) {
+//cout<<"Stack is Empty";
+return ('@');
 }
 else {
-// pop of all the operators from the stack and
-// append it to the postfix expression till we
-// see an operator with a lower precedence that
-// the current operator
-while (!s.empty() && s.gettop() != '(' &&
-priority <= getpriority(s.gettop())) {
-postfix[k++] = s.gettop();
-s.pop();
+temp = stk [top--]; //fetch topth position in temp
+return (temp);
+}}
+float stack :: popeval(){
+int status;
+float temp;
+status = IsEmptyeval ();
+if (status == -1) {
+//cout<<"Stack is Empty";
+return (-999);
 }
-// push the current operator onto stack
-s.push(ch);
+else {
+temp = stkeval [topeval--]; //fetch topth position in temp
+return (temp); }
 }
-}
-i++;
-} // pop of the remaining operators present in the stack
-// and append it to postfix expression
-while (!s.empty()) {
-postfix[k++] = s.gettop();
-s.pop();
-}
-postfix[k] = 0; // null terminate the postfix expression
-}
-int eval(int op1, int op2, char operate)// returns the value when a
-specific operator
-// operates on two operands
-{
-switch (operate) {
-case '*': return op2 * op1;case '/': return op2 / op1;
-case '+': return op2 + op1;
-case '-': return op2 - op1;
-default : return 0;
-}
-}
-// evaluates the postfix operation
-int evalPostfix(char postfix[], int size) {
-stack s;
-int i = 0;
-char ch;
-int val;
-while (i < size) {
-ch = postfix[i];
-if (isdigit(ch)) {// we saw an operand
-// push the digit onto stack
-s.push(ch-'0');
-}
-else { // we saw an operator
-// pop off the top two operands from the
-// stack and evalute them using the current
-// operator
-int op1 = int(s.gettop());
-s.pop();
-int op2 = int(s.gettop());
-s.pop();
-val = eval(op1, op2, ch);// push the value obtained after
-evaluating
-// onto the stack
-s.push(val);
-}
-i++;
-}
-return val;
-}
-// main
-int main() {
-char infix[20];
-cout<<"enter the infix expression";
+void stack :: read() {
+cout<<"Enter infix expression ";
 cin>>infix;
-int size = strlen(infix);
-char postfix[size];
-infix2postfix(infix,postfix,size);
-int val = evalPostfix(postfix, k);cout<<"\nInfix Expression :: "<<infix;
-cout<<"\nPostfix Expression :: "<<postfix;
-cout<<"\nExpression evaluates to "<<val;
-cout<<endl;
-cout<<endl;
+}
+void stack :: covert_infix_postfix() {
+int i,k=0;
+char sop;
+for(i=0;infix[i]!='\0';i++) {
+if(infix[i]>='0' && infix[i]<='9')
+postfix[k++] = infix[i];
+else if(infix[i]=='(')
+push(infix[i]);
+else if(infix[i]==')') {
+while((sop=pop())!='(')
+//pop all operators till ‘(’ doesn’t come
+postfix[k++]=sop;
+}
+else {
+while(priority(infix[i]) <= priority(sop=pop()))
+{
+postfix[k++] = sop;
+if(top==-1)
+break;
+}
+if(priority(infix[i]) > priority(sop))
+{
+push(sop); //re-push the oper taken for comparison
+}
+push(infix[i]); //push the operator from i/p to stack
+}
+}//for
+while(top!=-1) {
+sop = pop();
+postfix[k++] = sop;
+}
+postfix[k-1]='\0';
+cout<<endl<<"The postfix is : "<<postfix;
+}
+int stack :: priority(char x) {
+switch(x) {
+case '@':
+return -1;
+case '(':
+return 0;
+case '+':
+return 1;
+case '-':
+return 1;
+case '*':
+return 2;
+case '/':
+return 2;
+}
+}
+float stack :: evaluatepostfix()
+{
+float value,operand1,operand2,result;
+for(int i=0;postfix[i]!='\0';i++)
+{
+// Push operands onto the stack
+if(postfix[i]>='0' && postfix[i]<='9')
+{
+value = postfix[i] - 48;
+pusheval(value);
+}
+else
+{
+operand2=popeval();
+operand1=popeval();
+switch(postfix[i])
+{
+case '+':
+result=operand1+operand2;
+break;
+case '*':
+result=operand1*operand2;
+break;
+case '-':
+result=operand1-operand2;
+break;
+case '/':
+result=operand1/operand2;
+break;
+}
+pusheval(result);
+}
+}
+result=popeval();
+return result;
+}
+int main()
+{
+stack s;
+float answer;
+s.read();
+s.covert_infix_postfix();
+answer=s.evaluatepostfix();
+cout<<endl<<"The answer is : "<<answer;
 return 0;
 }
