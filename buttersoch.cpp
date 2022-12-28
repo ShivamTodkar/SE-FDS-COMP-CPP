d90a6010088a3670aa853ffa18f83b3b9573494c
ShyamTodkar <121312340+ShivamTodkar@users.noreply.github.com>
2022-12-28 20:04:47 +0530

    Update buttersoch.cpp

--- buttersoch.cpp
+++ buttersoch.cpp
@@ -1,235 +1,315 @@
-#include<iostream>
+#include <iomanip>
+#include <iostream>
 using namespace std;
-struct node
-{ int roll;
-struct node *next;
+
+class Student
+{
+	int rollNo;
+	string name;
+	Student *next;
+	friend class SecondYear;
+
+	public:
+	Student()
+	{
+		rollNo=-1;
+		name="";
+		next=NULL;
+	}
+
+	Student(int rollNo,string name)
+	{
+		next=NULL;
+		this->rollNo=rollNo;
+		this->name=name;
+	}
+
+	void printStudentData()
+	{
+		cout<<endl<<setw(7)<<rollNo<<setw(20)<<name;
+	}
 };
-class info
-{ node
-*head1=NULL,*temp1=NULL,*head2=NULL,*temp2=NULL,*head=NULL,*temp=NULL,*h1=NUL
-L,*head3=NULL,*temp3=NULL;
-int c,i,f,j,k;
-public:
-node *create();
-void insert();
-void allstud();
-void vanila();
-void butters();
-void uice();
-void nice();
-void notice();
-void ovanila();
-void obutters();
-void display();} ;
-node *info::create()
-{ node *p=new(struct node);
-cout<<"enter student rollno";
-cin>>c;
-p->roll=c;
-p->next=NULL;
-return p;
-}
-void info::insert()
+
+class SecondYear
 {
-node *p=create();
-if(head==NULL)
-{ head=p;
-}
-else
-{
-temp=head;
-while(temp->next!=NULL)
-{ temp=temp->next; }
-temp->next=p;
-}
-}
-void info::display()
-{ temp=head; while(temp-
->next!=NULL)
-{
-cout<<"\n"<<temp->roll;
-temp=temp->next;
-} cout<<"\n"<<temp->roll;
-}
-void info::allstud()
-{cout<<"enter no. of students";
-cin>>k;
-head=NULL;
-for(i=0;i<k;i++)
-{ insert();
-h1=head;
-} display();
-head=NULL;
-}
-void info::vanila()
-{cout<<"enter no. of students who like vanila";
-cin>>k;
-head=NULL;
-for(i=0;i<k;i++)
-{ insert();
-head1=head;
-} display();
-head=NULL;
-}
-void info::butters()
-{
-cout<<"enter no. of students who like butterscotch";
-cin>>j;
-for(i=0;i<j;i++)
-{ insert();
-head2=head;
-} display();
-head=NULL;
-}
-void info::uice()
-{ cout<<"students who like vanila or butterscotch\n";
-temp1=head1;
-while(temp1!=NULL)
-{
-node *p=new(struct node);
-p->roll=temp1->roll; p-
->next=NULL;
-if(head3==NULL)
-{ head3=p;
-}
-else
-{
-temp3=head3;
-while(temp3->next!=NULL)
-{ temp3=temp3->next; }
-temp3->next=p;
-}
-temp1=temp1->next;
-}
-temp2=head2;
-while(temp2!=NULL)
-{ f=0;temp1=head1;
-while(temp1!=NULL)
-{
-if(temp2->roll==temp1->roll)
-{ f=1;
-}
-temp1=temp1->next;
-}
-if(f==0)
-{
-node *p=new(struct node);
-p->roll=temp2->roll;
-p->next=NULL;
-if(head3==NULL)
-{ head3=p;
-}
-else
-{
-temp3=head3;
-while(temp3->next!=NULL)
-{ temp3=temp3->next; }
-temp3->next=p;
-}
-}
-temp2=temp2->next;
-}
-temp3=head3;
-while(temp3->next!=NULL)
-{ cout<<"\n"<<temp3->roll;
-temp3=temp3->next;
-} cout<<"\n"<<temp3->roll;
-}
-void info::ovanila()
-{
-cout<<"\nstudents like only vanila \n";
-temp1=head1;
-while(temp1!=NULL)
-{ temp2=head2; f=0;
-while(temp2!=NULL)
-{ if(temp1->roll==temp2->roll){ f=1;
-}
-temp2=temp2->next;
-}
-if(f==0)
-{ cout<<"\n"<<temp1->roll; }
-temp1=temp1->next;
-}
-}
-void info::obutters()
-{
-cout<<"\nstudents like only butterscotch\n";
-temp2=head2;
-while(temp2!=NULL)
-{ temp1=head1; f=0;
-while(temp1!=NULL)
-{ if(temp2->roll==temp1->roll)
-{ f=1;} temp1=temp1-
->next;
-}
-if(f==0)
-{ cout<<"\n"<<temp2->roll; }
-temp2=temp2->next;
-}
-}
-void info::nice()
-{
-cout<<"\nstudents who like both vanila and butterscotch\n";
-temp1=head1;
-while(temp1!=NULL)
-{ temp2=head2;
-while(temp2!=NULL)
-{ if(temp1->roll==temp2->roll) {
-cout<<"\n"<<temp1->roll; }
-temp2=temp2->next;
-}
-temp1=temp1->next;
-}}
-void info::notice()
-{
-cout<<"\nstudents who like neither vanila nor butterscotch\n";
-temp=h1;
-while(temp!=NULL)
-{ temp3=head3; f=0;
-while(temp3!=NULL)
-{ if(temp->roll==temp3->roll)
-{ f=1;}
-temp3=temp3->next;
-}
-if(f==0)
-{ cout<<"\n"<<temp->roll; }
-temp=temp->next;
-}
-}
+	Student *start;
+
+	public:
+	SecondYear()
+	{
+		start=NULL;
+	}
+
+	Student* getStart()
+	{
+		return start;
+	}
+
+	void AddStudent(int rollNo,string name)
+	{
+		Student *s=new Student(rollNo,name);
+		if(start==NULL)
+			start=s;
+		else
+		{
+			s->next=start;
+			start=s;
+		}
+	}
+
+	bool deleteStudent(int rollNo)
+	{
+		if(start==NULL)
+			return false;
+		else if(start->next==NULL)
+		{
+			Student *temp=start;
+			start=NULL;
+			delete temp;
+			return true;
+		}
+		else
+		{
+			Student *prev=NULL,*temp=start;
+			while(temp!=NULL)
+			{
+				if(temp->rollNo==rollNo)
+					break;
+				prev=temp;
+				temp=temp->next;
+			}
+			if(temp==NULL)
+				return false;
+			if(temp==start)
+				start=start->next;
+			else
+				prev->next=temp->next;
+			delete temp;
+			return true;
+		}
+	}
+
+	void onlySet(SecondYear *list2)
+	{
+		Student *temp2=list2->start;
+		Student *temp1=this->start;
+		while(temp1!=NULL)
+		{
+			bool isPresent=false;
+			temp2=list2->start;
+			while(temp2!=NULL)
+			{
+				if(temp1->rollNo==temp2->rollNo)
+				{
+					isPresent=true;
+					break;
+				}
+				temp2=temp2->next;
+			}
+			if(!isPresent)
+				temp1->printStudentData();
+			temp1=temp1->next;
+		}
+	}
+
+	SecondYear* unionSet(SecondYear *list2)
+	{
+		Student *temp1=start;
+		Student *temp2=list2->start;
+		SecondYear *temp3=new SecondYear;
+		while(temp1!=NULL)
+		{
+			//temp1->printStudentData();
+			temp3->AddStudent(temp1->rollNo,temp1->name);
+			temp1=temp1->next;
+		}
+		while(temp2!=NULL)
+		{
+			bool isPresent=false;
+			temp1=start;
+			while(temp1!=NULL)
+			{
+				if(temp1->rollNo==temp2->rollNo)
+				{
+					isPresent=true;
+					break;
+				}
+				temp1=temp1->next;
+			}
+			if(!isPresent)
+			{
+				//  temp2->printStudentData();
+				temp3->AddStudent(temp2->rollNo,temp2->name);
+			}
+			temp2=temp2->next;
+		}
+		return temp3;
+	}
+
+	void displayNone(SecondYear *unionSetAB)
+	{
+		Student *temp1=start;
+		Student *temp2=unionSetAB->start;
+
+		while(temp1!=NULL)
+		{
+			bool isPresent=false;
+			temp2=unionSetAB->start;
+			while(temp2!=NULL)
+			{
+				if(temp1->rollNo==temp2->rollNo)
+				{
+					isPresent=true;
+					break;
+				}
+				temp2=temp2->next;
+			}
+			if(!isPresent)
+				temp1->printStudentData();
+			temp1=temp1->next;
+		}
+
+	}
+
+	void intersection(SecondYear *list2)
+	{
+		Student *temp1=start;
+		Student *temp2=NULL;
+
+		while(temp1!=NULL)
+		{
+			temp2=list2->start;
+			while(temp2!=NULL)
+			{
+				if(temp1->rollNo==temp2->rollNo)
+				{
+					temp1->printStudentData();
+					break;
+				}
+				temp2=temp2->next;
+			}
+			temp1=temp1->next;
+		}
+	}
+
+	void displayStudents()
+	{
+		Student *temp=start;
+		cout<<endl<<setw(7)<<"RollNos" <<setw(20)<<"Names";
+		while(temp!=NULL)
+		{
+			temp->printStudentData();
+			temp=temp->next;
+		}
+
+	}
+
+};
+
 int main()
-{ info
-s; int i;
-char ch;
-do{
-cout<<"\n choice the options";
-cout<<"\n 1. To enter all students rollno ";
-cout<<"\n 2. To enter the rollno of student who like vanila";
-cout<<"\n 3. To enter the rollno of student who like butterscotch";
-cout<<"\n 4. To display the rollno of student who like vanila or butterscotch";
-cout<<"\n 5. To display the rollno of student who like only vanila";
-cout<<"\n 6. To display the rollno of student who like only butterscotch";
-cout<<"\n 7. To display the rollno of student who like both vanila and butterscotch ";
-cout<<"\n 8. To display the rollno of student who neither like vanila nor butterscotch";
-cin>>i;
-switch(i)
-{case 1: s.allstud(); break;case 2: s.vanila();
-break;
-case 3: s.butters();
-break;
-case 4: s.uice();
-break;
-case 5: s.ovanila();
-break;
-case 6: s. obutters();
-break;
-case 7: s.nice();
-break;
-case 8: s.notice();
-break;
-default: cout<<"\n unknown choice";
-}
-cout<<"\n do you want to continue enter y/Y \n";
-cin>>ch;
-}while(ch=='y'||ch=='Y');
-return 0;
+{
+	SecondYear *A,*B,*universalSet,*temp=NULL;
+	A=new SecondYear;
+	B=new SecondYear;
+	universalSet=new SecondYear;
+	int x=-1,opt,flav;
+	int rollNo;
+	string name;
+	bool del=false;
+
+	do
+	{
+		cout<<endl;
+		cout<<"************ Second Year Students Volatile Database *****************";
+		cout<<endl<<"1.Enter student\n2.Delete Student\n3.Display students\n4.Display Students(like-Vanilla)";
+		cout<<"\n5.Display Students(like-Butterscotch)"<<"\n6.Display Students(like-both Vanilla and ButterScotch)";
+		cout<<"\n7.Display Students(like-none of them)\n8.Display Students(like-either Vanilla or ButterScotch or Both)\n9.Exit";
+		cin>>opt;
+		switch(opt)
+		{
+		case 1:
+			cout<<"\nEnter the name of student ";
+			cin>>name;
+			cout<<"\nEnter RollNo";
+			cin>>rollNo;
+			cout<<"\nEnter the flavour liked:\n1.ButterScotch\n2.Vanilla\n3.Both\n4.None";
+			cin>>flav;
+
+				switch(flav)
+				{
+				case 1:B->AddStudent(rollNo,name);
+				break;
+
+				case 2:A->AddStudent(rollNo,name);
+				break;
+
+				case 3:A->AddStudent(rollNo,name);
+				B->AddStudent(rollNo,name);
+				break;
+
+				case 4:break;
+
+				default:"Wrong option";
+				}
+
+				if(flav>=1 && flav<=4)
+					universalSet->AddStudent(rollNo,name);
+			break;
+
+		case 2:
+			cout<<"Enter rollNo";
+			cin>>rollNo;
+			del=false;
+			if(A->deleteStudent(rollNo))
+				{
+					universalSet->deleteStudent(rollNo);
+					del=true;
+				}
+			if(B->deleteStudent(rollNo))
+				{
+					universalSet->deleteStudent(rollNo);
+					del=true;
+				}
+			if(universalSet->deleteStudent(rollNo))
+					del=true;
+				if(!del)
+					cout<<endl<<"No such student";
+				else
+					cout<<endl<<"Deleted from database";
+			break;
+
+		case 3:
+				universalSet->displayStudents();
+				break;
+
+		case 4:
+				A->onlySet(B);
+				break;
+
+		case 5:
+				B->onlySet(A);
+				break;
+
+		case 6:
+				A->intersection(B);
+				break;
+
+		case 7:
+				temp=A->unionSet(B);
+				universalSet->displayNone(temp);
+				delete temp;
+				break;
+
+		case 8:temp=A->unionSet(B);
+			temp->displayStudents();
+			delete temp;
+			break;
+
+		case 9:break;
+
+		default:cout<<"Wrong option";
+				break;
+		}
+	}while(x!=9);
+
 }
